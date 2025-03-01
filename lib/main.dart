73d23164b4aa319e48fad5459576328eece01778
Manuel <caballeromanuel203@gmail.com>
2025-03-01 20:47:19 +0800

    created domain models and repository for the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,16 @@
-import 'package:carte/config/routes_config.dart';
-import 'package:carte/config/theme_config.dart';
-import 'package:carte/controllers/auth_controller.dart';
-import 'package:carte/hive/hive_registrar.g.dart';
-import 'package:carte/models/user.dart';
-import 'package:carte/views/login_page.dart';
 import 'package:flutter/material.dart';
-import 'package:hive_ce_flutter/hive_flutter.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:provider/provider.dart';
-import 'package:uuid/v4.dart';
 
-void main(List<String> args) async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  final appDocumentDirectory = await getApplicationDocumentsDirectory();
-
-  await Hive.initFlutter(appDocumentDirectory.path);
-
-  Hive.registerAdapters();
-
-  await Hive.openBox<User>('userBox');
-  await Hive.openBox<UuidV4>('authBox');
-
-  runApp(ChangeNotifierProvider(
-    create: (context) => AuthController(),
-    child: const Main(),
-  ));
+void main() async {
+  runApp(const App());
 }
 
-class Main extends StatelessWidget {
-  const Main({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
-      theme: themeData,
-      routes: routes,
     );
   }
 }
--- lib/utils/input_editing_controller.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-class InputEditingController extends TextEditingController {
-  String? errorText;
-}
